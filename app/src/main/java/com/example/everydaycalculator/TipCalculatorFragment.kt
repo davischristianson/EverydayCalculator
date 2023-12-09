89c47a73f8a36689405f86f7510f874538968e3b
davischristianson <dav.christian10@gmail.com>
2023-12-08 16:47:19 -0800

    Fixed and finished code for tip calculator fragment

--- app/src/main/java/com/example/everydaycalculator/TipCalculatorFragment.kt
+++ app/src/main/java/com/example/everydaycalculator/TipCalculatorFragment.kt
@@ -5,12 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.math.BigDecimal
 
 class TipCalculatorFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_tip_calculator, container, false)
+        val view = inflater.inflate(R.layout.fragment_tip_calculator, container, false)
+
+        val calculateButton = view.findViewById<Button>(R.id.btnCalculateTip)
+
+        calculateButton.setOnClickListener {
+            val amount = view.findViewById<EditText>(R.id.etTotalAmount).getText().toString()
+            val tipPercent = view.findViewById<EditText>(R.id.etTipPercent).getText().toString()
+            val totalAmount = BigDecimal(amount.toDouble() *
+                    (tipPercent.toDouble() / 100))
+            val finalAmount = view.findViewById<TextView>(R.id.tvTipAmount)
+            finalAmount.setText("$" + "%.2f".format(totalAmount))
+        }
+        return view
     }
 }
